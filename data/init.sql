b346e84f01d7b3da7df914db1f15cafb18085ca9
cmatthia <cmatthia@sfu.ca>
2016-12-05 16:02:27 -0800

    secured DB with admin account, setup rbenv vars

--- data/init.sql
+++ data/init.sql
@@ -1,3 +1,5 @@
 CREATE DATABASE IF NOT EXISTS noted_development;
 CREATE DATABASE IF NOT EXISTS noted_test;
 CREATE DATABASE IF NOT EXISTS noted_production;
+CREATE USER 'noted_admin'@'localhost' IDENTIFIED BY 'cmpt470group14';
+GRANT ALL PRIVILEGES ON *.* TO 'noted_admin'@'localhost'
